56fe37212ad3aa9aff588a75a90a0687c908a783
Yuvraj <yuvrajbhinchar00@gmail.com>
2024-07-24 21:20:18 -0700

    fetch api & add weather icon

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,53 +1,66 @@
+import { useState } from "react";
+import { WeatherSvg } from "weather-icons-animated";
 function Weather(){
+  const api_key = "deb56c7b018d04081faa707eb8c68ac6"
+  const [WeatherData, setWeatherData] = useState("")
+  const [weather,setWeather] = useState("")
+  function searchWeather(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${WeatherData}&appid=${api_key}&units=metric`).then((res)=>{
+      return res.json()
+      }).then((data)=>{
+        console.log(data)
+        setWeather(data)
+      })
+  }
     return(
         <>
         {/* Main Screen */}
-        <div className="w-screen h-screen items-center bg-cover bg-no-repeat flex justify-center">
+        <div className="w-screen h-screen items-center bg-cover bg-no-repeat justify-center flex bg-Thundercloud">
         {/* Main Div */}
-        <div className="w-3/5 h-4/5 bg-[#000000b8;] rounded-lg flex">
+        <div className="w-3/5 h-4/5 rounded-lg md:flex">
             {/* Left Panel */}
-           <div className="w-1/2 bg-left h-full bg-cover bg-no-repeat rounded-l-lg flex flex-col justify-between ">
+           <div className="w-1/2 bg-left h-full bg-cover bg-no-repeat rounded-l-lg flex flex-col justify-between bg-[#fbfafa49] ">
             {/* 1 */}
             <div className="flex justify-end m-4">
-                <p className="font-bold text-white text-2xl"> Jaipur India</p>
+                <p className="font-bold text-white text-2xl">{weather.name}  {weather.sys && weather.sys.country}</p>
             </div>
             {/* 2 */}
-            <div className="flex justify-center items-center"><img  src="#" className="rounded-full h-2/5 bg-[#ffffff85]" alt="" /></div>
+            <div className="flex justify-center items-center"><img  src={`https://openweathermap.org/img/wn/${weather.weather && weather.weather[0].icon}@2x.png`} alt={"https://www.popsci.com/wp-content/uploads/2018/12/10/QKOPUFKJMVV7JE2YNBCV2FILFQ.jpg"} className="rounded-full h-4/5 bg-[#ffffff85]" /></div>
             {/* 3 */}
             <div className="flex justify-between items-end m-4">
                 <div className="font-bold text-xl text-gray-200">
-                    <p>45</p>
-                    <p>54</p>
+                    <p>{weather.coord && weather.coord.lat}</p>
+                    <p>{weather.coord && weather.coord.lon}</p>
                 </div>
-                <div className="font-bold text-2xl text-gray-200"><p>2353</p></div>
+                <div className="font-bold text-2xl text-gray-200"><p>{weather.main && weather.main.temp}°C</p></div>
             </div>
             </div>
             {/* Right Panel */}
-            <div className="w-1/2 h-full">
-            <div className="h-1/5 flex justify-center items-center border-b border-gray-300 m-4"></div>
-            <div className="flex border border-gray-200 rounded-lg w-3/5 mx-auto"><input type="search" placeholder="Search" className="bg-transparent  text-white placeholder-white border-none outline-none px-8 py-2"/>
+            <div className="w-1/2 h-full bg-[#000000b8;] ">            
+            <div className="h-1/5 flex justify-center items-center border-b border-gray-300 m-4"><WeatherSvg state="sunny" width={100} height={100}></WeatherSvg></div>
+            <div className="flex border border-gray-200 rounded-lg w-3/5 mx-auto"><input type="search" placeholder="Search By Only City" className="bg-transparent  text-white placeholder-white border-none outline-none px-8 py-2" value={WeatherData} onChange={(e)=>{setWeatherData(e.target.value)} }/>
                 <span
                   className="material-symbols-outlined text-2xl cursor-pointer text-white 
-                 rounded-full p-1 float-right "
+                 rounded-full p-1 float-right " onClick={searchWeather}
                 >
                   search
                 </span>
             </div>
             <div className="text-center text-white font-semibold my-5">
-                <p>Jaipur,India</p>
-                <p>Halllo</p>
+                <p>{weather.name} {weather.sys && weather.sys.country}</p>
+                <p>{weather.weather && weather.weather[0].description}</p>
               </div>
               <div className="flex justify-around font-bold text-white border-b border-gray-300 m-6 p-2">
                   <p>Temp</p>
-                  <p>78°C</p>
+                  <p>{weather.main && weather.main.temp}°C</p>
                 </div>
                 <div className="flex justify-around font-bold text-white border-b border-gray-300 m-6 p-2">
                   <p>Visibility</p>
-                  <p>879Km</p>
+                  <p>{weather.visibility}Km</p>
                 </div>
                 <div className="flex justify-around font-bold text-white border-b border-gray-300 m-6 p-2">
                   <p>Wind Speed</p>
-                  <p>90 meter/sec</p>
+                  <p>{weather.wind && weather.wind.speed} meter/sec</p>
                 </div>
             </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'Thundercloud':"url('https://t3.ftcdn.net/jpg/05/64/04/18/360_F_564041870_iGZq5SD8qI9SU6VkXzH0xL91IsC71iHH.jpg')"
+      }
+    },
   },
   plugins: [],
 }
